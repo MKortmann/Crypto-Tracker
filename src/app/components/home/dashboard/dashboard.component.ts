2695667ea2171e37af0f042553a910157e7dbac8
MKortmann <companymvms@gmail.com>
2020-05-22 21:33:09 +0200

    add: charts to test - TODO: add real data to graphics

--- src/app/components/home/dashboard/dashboard.component.ts
+++ src/app/components/home/dashboard/dashboard.component.ts
@@ -11,12 +11,40 @@ import { Coin } from '../../../models/Coin';
 export class DashboardComponent implements OnInit {
   data: any;
   dataBar: any;
+  dataDoughnut: any;
+  dataPolar: any;
   labels: any;
   coins: Coin[];
 
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
+    this.dataPolar = {
+      datasets: [
+        {
+          data: [11, 16, 7, 3, 14],
+          backgroundColor: [
+            '#FF6384',
+            '#4BC0C0',
+            '#FFCE56',
+            '#E7E9ED',
+            '#36A2EB',
+          ],
+          label: 'My dataset',
+        },
+      ],
+      labels: ['Red', 'Green', 'Yellow', 'Grey', 'Blue'],
+    };
+    this.dataDoughnut = {
+      labels: ['A', 'B', 'C'],
+      datasets: [
+        {
+          data: [300, 50, 100],
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+          hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+        },
+      ],
+    };
     this.dataService.getGlobalCryptoData(0, 7).then((res) => {
       this.labels = res.data.map((data) => data.name);
       console.log(this.labels);
@@ -69,10 +97,12 @@ export class DashboardComponent implements OnInit {
         {
           label: 'First Dataset',
           data: [65, 59, 80, 81, 56, 55, 40],
+          borderColor: '#1E88E5',
         },
         {
           label: 'Second Dataset',
           data: [28, 48, 40, 19, 86, 27, 90],
+          borderColor: '#7CB342',
         },
       ],
     };
