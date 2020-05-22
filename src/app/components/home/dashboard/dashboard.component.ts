6bde6eb6315bbaf5d7368fd6de4e747ff6d2b953
MKortmann <companymvms@gmail.com>
2020-05-22 21:25:51 +0200

    add: adding flex-grid for tables - TODO: fix bar chart

--- src/app/components/home/dashboard/dashboard.component.ts
+++ src/app/components/home/dashboard/dashboard.component.ts
@@ -26,13 +26,19 @@ export class DashboardComponent implements OnInit {
         labels: [...this.labels],
         datasets: [
           {
-            label: 'My First dataset',
+            label: 'Change 1h',
             backgroundColor: '#42A5F5',
             borderColor: '#1E88E5',
             data: [65, 59, 80, 81, 56, 55, 40],
           },
           {
-            label: 'My Second dataset',
+            label: 'Change 24h',
+            backgroundColor: '#9CCC65',
+            borderColor: '#7CB342',
+            data: [28, 48, 40, 19, 86, 27, 90],
+          },
+          {
+            label: 'Change 7 days',
             backgroundColor: '#9CCC65',
             borderColor: '#7CB342',
             data: [28, 48, 40, 19, 86, 27, 90],
