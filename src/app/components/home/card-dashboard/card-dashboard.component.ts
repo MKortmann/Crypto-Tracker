615f2b6d5f658c004b75033bc77adfbf2fc4e17e
MKortmann <companymvms@gmail.com>
2020-05-23 14:56:48 +0200

    add: cards-dashboard

--- src/app/components/home/card-dashboard/card-dashboard.component.ts
+++ src/app/components/home/card-dashboard/card-dashboard.component.ts
@@ -1,12 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
+import { DataService } from '../../../services/data.service';
+
 @Component({
   selector: 'app-card-dashboard',
   templateUrl: './card-dashboard.component.html',
   styleUrls: ['./card-dashboard.component.css'],
 })
 export class CardDashboardComponent implements OnInit {
-  constructor() {}
+  // labels: any;
+  // prices: any;
+  data: any;
 
-  ngOnInit(): void {}
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(): void {
+    this.dataService.getGlobalCryptoData(0, 12).then((res) => {
+      // this.labels = res.data.map((data) => data.name);
+      // this.prices = res.data.map((data) => data.price);
+      this.data = res.data;
+      console.log(this.data);
+    });
+  }
 }
