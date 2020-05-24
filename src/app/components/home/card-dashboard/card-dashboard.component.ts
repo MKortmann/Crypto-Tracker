c41ba55db58b4b5c875898f0f5a347958cc743ad
MKortmann <companymvms@gmail.com>
2020-05-24 10:39:03 +0200

    add: EUR value for dashboard-cards

--- src/app/components/home/card-dashboard/card-dashboard.component.ts
+++ src/app/components/home/card-dashboard/card-dashboard.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 import { DataService } from '../../../services/data.service';
 
+import { ExchangeService } from '../../../services/exchange.service';
+
 @Component({
   selector: 'app-card-dashboard',
   templateUrl: './card-dashboard.component.html',
@@ -11,15 +13,30 @@ export class CardDashboardComponent implements OnInit {
   // labels: any;
   // prices: any;
   data: any;
+  selectRateEUR: number;
 
-  constructor(private dataService: DataService) {}
+  constructor(
+    private dataService: DataService,
+    private exchangeService: ExchangeService
+  ) {}
 
   ngOnInit(): void {
     this.dataService.getGlobalCryptoData(0, 12).then((res) => {
       // this.labels = res.data.map((data) => data.name);
       // this.prices = res.data.map((data) => data.price);
-      this.data = res.data;
+      this.data = [...res.data];
       console.log(this.data);
+
+      this.exchangeService.getMoney('USD').then((res) => {
+        this.selectRateEUR = res.rates['EUR'];
+        console.log(this.selectRateEUR);
+
+        this.data.forEach((item) => {
+          item['price_eur'] = item.price_usd * res.rates['EUR'];
+          // console.log(item);
+        });
+        console.log(this.data);
+      });
     });
   }
 }
