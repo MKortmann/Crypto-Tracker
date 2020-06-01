581c74531b5cf02477dba25045ee2bfad64501c1
MKortmann <companymvms@gmail.com>
2020-06-01 20:23:50 +0200

    Auto-generated commit

---
+++ boilerplate/assets/i18n/de.ts
@@ -0,0 +1,51 @@
+export const de = {
+  TRANSLATE: {
+    NAVBAR: {
+      TITLE: 'Crypto-Tracker',
+      HOME: 'Start',
+      ABOUT: 'Über',
+      NAV_2: 'Nav_2',
+      REGISTER: 'Registrieren',
+      LOGIN: 'Anmeldung',
+    },
+    HOME: {
+      TITLE: 'Start',
+      TABLE: {
+        RANK: 'Rang',
+        SYMBOL: 'Symbol',
+        NAME: 'Name',
+        PRICE: 'Preis in USD',
+        PRICE_EUR: 'Preis in EUR',
+        PRICE_BTC: 'Preis in BTC',
+        CHANGE_1H: 'Veränderung 1h',
+        CHANGE_24H: 'Veränderung 24h',
+        CHANGE_7D: 'Veränderung 7 Tage',
+        MARKET_CAP: 'Market Kap USD',
+        VOLUME_24H: 'Volumen 24h',
+        VOLUME_24HA: 'Volumen 24ha',
+      },
+      GRAPHIC_CARD: {
+        CRYPTOS: 'Summe der Kryptowährungen',
+        ACTIVE_MARKETS: 'Aktive Märkte',
+        TOTAL_OF_MARKET_CAP: 'Gesamtmarktkapitalisierung',
+        TOTAL_OF_VOLUME: 'Gesamtvolumen in 24h',
+        CRYPTO_DOMINANCE: 'Kryptodominanz Prozentsatz',
+      },
+    },
+    ABOUT: {
+      TITLE: 'Über',
+      COMMENTS:
+        'Diese Crypto-Tracker-App hat das Hauptziel, die Entwicklung verschiedener Crypto-Münzen zu verfolgen (derzeit etwa 200). Ich hoffe du genießt es!',
+    },
+    REGISTER: {
+      TITLE: 'Registrieren',
+    },
+    LOGIN: {
+      TITLE: 'Anmeldung',
+    },
+    FOOTER: {
+      LABEL_LEFT: 'Marcelo Kortmann',
+      LABEL_MIDDLE: 'Version 1.0.0',
+    },
+  },
+};
---
+++ boilerplate/assets/i18n/en.ts
@@ -0,0 +1,51 @@
+export const en = {
+  TRANSLATE: {
+    NAVBAR: {
+      TITLE: 'Crypto-Tracker',
+      HOME: 'Home',
+      ABOUT: 'About',
+      NAV_2: 'Nav_2',
+      REGISTER: 'Register',
+      LOGIN: 'Login',
+    },
+    HOME: {
+      TITLE: 'Home',
+      TABLE: {
+        RANK: 'Rank',
+        SYMBOL: 'Symbol',
+        NAME: 'Name',
+        PRICE: 'Price in USD',
+        PRICE_EUR: 'Price in EUR',
+        PRICE_BTC: 'Price in BTC',
+        CHANGE_1H: 'Change 1h',
+        CHANGE_24H: 'Change 24h',
+        CHANGE_7D: 'Change 7 Days',
+        MARKET_CAP: 'Market Cap USD',
+        VOLUME_24H: 'Volume 24h',
+        VOLUME_24HA: 'Volume 24ha',
+      },
+      GRAPHIC_CARD: {
+        CRYPTOS: 'Total of Cryptocurrencies',
+        ACTIVE_MARKETS: 'Active Markets',
+        TOTAL_OF_MARKET_CAP: 'Total of Market Cap',
+        TOTAL_OF_VOLUME: 'Total Volume in 24h ',
+        CRYPTO_DOMINANCE: 'Crypto Dominance Percentage',
+      },
+    },
+    REGISTER: {
+      TITLE: 'REGISTER',
+    },
+    LOGIN: {
+      TITLE: 'Anmeldung',
+    },
+    ABOUT: {
+      TITLE: 'About',
+      COMMENTS:
+        'This Crypto-Tracker App has the main goal to track the development of different crypto-coins (at the moment about 200). I hope you enjoy it!',
+    },
+    FOOTER: {
+      LABEL_LEFT: 'Marcelo Kortmann',
+      LABEL_MIDDLE: 'Version 1.0.0',
+    },
+  },
+};
