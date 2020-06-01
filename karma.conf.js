e893787c91e0ccb4bdec1fe03758b8d7be69d666
MKortmann <companymvms@gmail.com>
2020-06-01 20:56:42 +0200

    code refactoring for deploy

--- karma.conf.js
+++ karma.conf.js
@@ -28,7 +28,7 @@ module.exports = function (config) {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/ticket-tracker'),
+      dir: require('path').join(__dirname, './coverage/crypto-tracker'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true,
     },
