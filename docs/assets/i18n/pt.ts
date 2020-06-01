8a2a78d8fc6ae1d342c2acf4b56d73f16a3fc0de
MKortmann <companymvms@gmail.com>
2020-06-01 20:37:19 +0200

    add: docs as dist

---
+++ docs/assets/i18n/pt.ts
@@ -0,0 +1,51 @@
+export const pt = {
+  TRANSLATE: {
+    NAVBAR: {
+      TITLE: 'Crypto-Tracker',
+      HOME: 'Home',
+      ABOUT: 'Sobre',
+      NAV_2: 'Nav_2',
+      REGISTER: 'Cadastro',
+      LOGIN: 'Conecte-se',
+    },
+    HOME: {
+      TITLE: 'Início',
+      TABLE: {
+        RANK: 'Posição',
+        SYMBOL: 'Símbolo',
+        NAME: 'Nome',
+        PRICE: 'Preço em USD',
+        PRICE_EUR: 'Preço em EUR',
+        PRICE_BTC: 'Preço em BTC',
+        CHANGE_1H: ' 1h',
+        CHANGE_24H: 'Mudança em 24h',
+        CHANGE_7D: 'Mudança 7 Days',
+        MARKET_CAP: 'Mercado Cap USD',
+        VOLUME_24H: 'Volume 24h',
+        VOLUME_24HA: 'Volume 24ha',
+      },
+      GRAPHIC_CARD: {
+        CRYPTOS: 'Número Total de Criptomoedas',
+        ACTIVE_MARKETS: 'Mercados Ativos',
+        TOTAL_OF_MARKET_CAP: 'Valor Total de Mercado',
+        TOTAL_OF_VOLUME: 'Volume Total em 24h ',
+        CRYPTO_DOMINANCE: 'Domínio de Criptomoedas ',
+      },
+    },
+    REGISTER: {
+      TITLE: 'REGISTRO',
+    },
+    LOGIN: {
+      TITLE: 'Conecte-se',
+    },
+    ABOUT: {
+      TITLE: 'Sobre',
+      COMMENTS:
+        'Este aplicativo Crypto-Tracker tem o objetivo principal de acompanhar o desenvolvimento de diferentes moedas criptográficas (atualmente, cerca de 200). Espero que você goste!',
+    },
+    FOOTER: {
+      LABEL_LEFT: 'Marcelo Kortmann',
+      LABEL_MIDDLE: 'Versão 1.0.0',
+    },
+  },
+};
