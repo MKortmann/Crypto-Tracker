caa96801f1b073787bee370bc0b8a07967671d2e
MKortmann <companymvms@gmail.com>
2020-07-29 21:53:35 +0200

    add: improved design + .travis.yml

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
+
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
@@ -7,11 +9,42 @@ import { TranslateService } from '@ngx-translate/core';
   styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
-  constructor(private translate: TranslateService) {}
+  constructor(
+    @Inject(DOCUMENT) private document: Document,
+    private translate: TranslateService
+  ) {}
+  darkMode = false;
 
   ngOnInit(): void {}
 
   change(code: string) {
     this.translate.use(code);
+  }
+
+  // this would allow us to load the theme at runtime, based on users preferences
+  loadStyle(styleName: string = 'dark') {
+    if (this.darkMode === false) {
+      this.darkMode = true;
+      styleName = 'dark';
+    } else {
+      this.darkMode = false;
+      styleName = 'light';
+    }
+    // get the head element of the page
+    const head = this.document.getElementsByTagName('head')[0];
+
+    const themeLink = this.document.getElementById(
+      'client-theme'
+    ) as HTMLLinkElement;
+
+    if (themeLink) {
+      themeLink.href = styleName + '.css';
+    } else {
+      const style = this.document.createElement('link');
+      style.id = 'client-theme';
+      style.rel = 'stylesheet';
+      style.href = `${styleName}.css`;
+      head.appendChild(style);
+    }
   }
 }
