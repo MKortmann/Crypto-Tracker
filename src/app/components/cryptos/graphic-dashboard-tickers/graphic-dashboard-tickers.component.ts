9c4c25707d4bd7d6883bdbfcbd5ae5575a985d19
MKortmann <companymvms@gmail.com>
2020-08-13 23:19:03 +0200

    add: graph for the last 24 hours coin tickers

--- src/app/components/cryptos/graphic-dashboard-tickers/graphic-dashboard-tickers.component.ts
+++ src/app/components/cryptos/graphic-dashboard-tickers/graphic-dashboard-tickers.component.ts
@@ -17,27 +17,49 @@ export class GraphicDashboardTickersComponent implements OnInit {
   labels = ['january', 'february', 'march'];
   data = [30, 60, 100];
 
+  coinName = 'btc-bitcoin';
+  symbol = 'btc';
+
   ngOnInit(): void {
-    this.coinPaprikaService.onSelectedCoinChange.subscribe(() => {
-      console.log('coin changed!');
-    });
-    this.getCoinByTicker();
+    this.coinPaprikaService.onSelectedCoinChange.subscribe(
+      (name) => {
+        const nameTemp = name.split('-');
+        if (nameTemp[2] !== undefined) {
+          this.coinName = `${nameTemp[1]} ${nameTemp[2]}`;
+        } else {
+          this.coinName = nameTemp[1];
+        }
+        this.symbol = nameTemp[0];
+        this.coinName = nameTemp[1];
+        console.log('coin changed!');
+      },
+      (error) => console.log(error)
+    );
+
+    const endDate = new Date();
+    endDate.setDate(endDate.getDate() + 1);
+
+    const startDate = new Date();
+    // startDate.setDate(startDate.getDate());
+
+    const endDateStr = endDate.toISOString().split('T')[0];
+    const startDateStr = startDate.toISOString().split('T')[0];
+
+    this.getCoinByTicker(this.coinName, startDateStr, endDateStr);
   }
 
-  getCoinByTicker(
-    coin = 'btc-bitcoin',
-    start = '2019-01-01',
-    end = '2019-01-20'
-  ) {
-    this.coinPaprikaService
-      .selectedCoinByTickers(coin, start, end)
-      .subscribe((res) => {
+  getCoinByTicker(coin, start, end) {
+    this.coinPaprikaService.selectedCoinByTickers(coin, start, end).subscribe(
+      (res) => {
+        console.log(res);
         const labels = res.map((item) => item.timestamp);
         const price = res.map((item) => item.price);
         const volume24h = res.map((item) => item.timestamp);
         const marketCap = res.map((item) => item.marketCap);
         this.graph(labels, price, volume24h, marketCap);
-      });
+      },
+      (error) => console.log(error)
+    );
   }
 
   graph(labels, price, volume24h, marketCap) {
@@ -47,6 +69,7 @@ export class GraphicDashboardTickersComponent implements OnInit {
         labels,
         datasets: [
           {
+            label: `${this.symbol}`,
             data: price,
             borderColor: 'rgba(255, 0, 0, 1)',
             fill: false,
@@ -65,13 +88,42 @@ export class GraphicDashboardTickersComponent implements OnInit {
       },
       options: {
         maintainAspectRatio: false,
+        tooltips: {
+          titleFontSize: 18,
+          bodyFontSize: 16,
+          // backgroundColor: '#4bc0c0',
+          backgroundColor: 'rgba(0, 0, 0, 0.8)',
+          callbacks: {
+            title: (tooltipItems, data) => {
+              let returnValue = tooltipItems[0].xLabel.split('T')[0];
+              returnValue = returnValue.split('-');
+              returnValue = `${returnValue[2]}-${returnValue[1]}-${returnValue[0]}`;
+              return returnValue;
+            },
+            label: (tooltipItem, data) => {
+              const label = data.datasets[tooltipItem.datasetIndex].label || '';
+              return `${label}: ${tooltipItem.yLabel}`;
+            },
+          },
+        },
         legend: {
-          display: false,
+          display: true,
+          fontSize: 16,
         },
         scales: {
           xAxes: [
             {
-              display: true,
+              gridLines: {
+                color: '#4bc0c0',
+                zeroLineColor: '#4bc0c0',
+              },
+              type: 'time',
+              ticks: {
+                fontColor: '#4bc0c0',
+              },
+              time: {
+                unit: 'hour',
+              },
             },
           ],
           yAxes: [
