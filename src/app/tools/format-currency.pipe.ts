b1c97e8cbcd02ab2de8ffaf436387aaeb83b5f3a
MKortmann <companymvms@gmail.com>
2020-08-20 23:23:12 +0200

    style: formating currency pipe numbers

--- src/app/tools/format-currency.pipe.ts
+++ src/app/tools/format-currency.pipe.ts
@@ -5,13 +5,13 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class FormatCurrencyPipe implements PipeTransform {
   transform(value: number, ...args: unknown[]): unknown {
-    if (value > 10 ** 3 && value <= 10 ** 6) {
+    if (value > 10 ** 5 && value <= 10 ** 8) {
       return `${Math.round(value / 10 ** 3)} K`;
-    } else if (value > 10 ** 6 && value <= 10 ** 9) {
+    } else if (value > 10 ** 8 && value <= 10 ** 11) {
       return `${Math.round(value / 10 ** 6)} M`;
-    } else if (value > 10 ** 9 && value <= 10 ** 12) {
+    } else if (value > 10 ** 11 && value <= 10 ** 14) {
       return `${Math.round(value / 10 ** 9)} B`;
-    } else if (value > 10 ** 12 && value <= 10 ** 15) {
+    } else if (value > 10 ** 14 && value <= 10 ** 17) {
       return `${Math.round(value / 10 ** 9)} T`;
     }
     // console.log(value, args);
