acb23fac307a3cd45dbd39afd9f506bbca387190
MKortmann <companymvms@gmail.com>
2020-08-23 11:24:56 +0200

    add: feed news - test phase

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as xml2js from 'xml2js';
+import { NewsRss } from '../../models/news-rss';
 
 @Component({
   selector: 'app-news',
@@ -6,7 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./news.component.scss'],
 })
 export class NewsComponent implements OnInit {
-  constructor() {}
+  RssData: NewsRss;
+  newsBitcoin = 'https://news.bitcoin.com/feed/';
 
-  ngOnInit(): void {}
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.getNewsFeed();
+  }
+
+  getNewsFeed() {
+    const requestOptions: object = {
+      observe: 'body',
+      responseType: 'text',
+    };
+    this.http.get<any>(this.newsBitcoin, requestOptions).subscribe((data) => {
+      xml2js.parseString(data, (error, result: NewsRss) => {
+        this.RssData = result;
+        console.log(result);
+      });
+    });
+  }
 }
