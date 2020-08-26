1f16a83785e251703bfacb4e23db72d99a93db3e
MKortmann <companymvms@gmail.com>
2020-08-26 17:54:46 +0200

    bug: fix sidebar icons + refactoring

--- src/app/components/news/single-feed/single-feed.component.ts
+++ src/app/components/news/single-feed/single-feed.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-single-feed',
@@ -14,7 +14,13 @@ export class SingleFeedComponent implements OnInit {
     url: 'https%3A%2F%2Fwww.globalcryptopress.com%2Ffeeds%2Fposts%2Fdefault',
     bookmark: false,
   };
+  @Output() clickedBookmark = new EventEmitter<string>();
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  sendTriggerToParent($event) {
+    this.clickedBookmark.emit($event);
+  }
 }
