1d77839ed0795d9ca3783b4bc08f74b547fbf517
MKortmann <companymvms@gmail.com>
2020-08-30 12:12:12 +0200

    bug: fix test service

--- src/app/services/feed-news.service.spec.ts
+++ src/app/services/feed-news.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
+import { HttpClient, HttpHandler } from '@angular/common/http';
+
 import { FeedNewsService } from './feed-news.service';
 
 describe('FeedNewsService', () => {
   let service: FeedNewsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler],
+    });
     service = TestBed.inject(FeedNewsService);
   });
 
