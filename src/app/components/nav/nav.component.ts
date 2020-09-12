84912ead472e8e7fef79d6fb980f8486bf1c73d0
MKortmann <companymvms@gmail.com>
2020-09-12 17:07:18 +0200

    add: angular transition and complete custom nav

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,23 +1,39 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
-import { DOCUMENT } from '@angular/common';
+
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
+  animations: [
+    trigger('flyInOut', [
+      state('in', style({ transform: 'translateX(0)' })),
+      transition('void => *', [
+        style({ transform: 'translateX(-100%)' }),
+        animate(100),
+      ]),
+      transition('* => void', [
+        animate(100, style({ transform: 'translateX(100%)' })),
+      ]),
+    ]),
+  ],
 })
 export class NavComponent implements OnInit {
-  darkMode = false;
+  public isShown = false;
 
-  constructor(
-    @Inject(DOCUMENT) private document: Document,
-    private translate: TranslateService
-  ) {}
+  constructor(private translate: TranslateService) {}
 
   ngOnInit(): void {}
 
   showSideNav() {
-    this.document.querySelector('.sidenav').classList.toggle('open');
+    this.isShown = !this.isShown;
   }
 }
